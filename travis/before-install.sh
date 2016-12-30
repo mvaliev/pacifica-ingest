2e9b283d7b3a28cf8f9ee76ce32ee64d33399fd8
dmlb2000 <dmlb2000@gmail.com>
2016-12-30 15:15:24 -0800

    add the wait loop for the ingest of metadata

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -5,6 +5,13 @@ sudo service mysql stop
 if [ -z "$RUN_LINTS" ]; then
   docker-compose up -d
   docker-compose stop ingestbackend ingestfrontend
+  MAX_TRIES=60
+  HTTP_CODE=$(curl -sL -w "%{http_code}\\n" localhost:8121/keys -o /dev/null || true)
+  while [[ $HTTP_CODE != 200 && $MAX_TRIES > 0 ]] ; do
+    sleep 1
+    HTTP_CODE=$(curl -sL -w "%{http_code}\\n" localhost:8121/keys -o /dev/null || true)
+    MAX_TRIES=$(( MAX_TRIES - 1 ))
+  done
   docker run -it --rm --net=pacificaingest_default  -e METADATA_URL=http://metadataserver:8121 -e PYTHONPATH=/usr/src/app --link /metadataserver:/metadataserver/metadataserver pacifica/metadata python test_files/loadit.py
 fi
 case "$TRAVIS_PYTHON_VERSION" in
