e9787fbeb1e4df75d9f3f78e474c59f77501f990
David Brown <dmlb2000@gmail.com>
2017-01-25 13:37:34 -0800

    fix #13 add more options to define user, password and vhost (#14)
    * fix #13 add more options to define user, password and vhost
    
    * apparently pass is a keyword that's important to format

--- settings.py
+++ settings.py
@@ -4,7 +4,16 @@ from os import getenv
 
 BROKER_SERVER = getenv('BROKER_SERVER', '127.0.0.1')
 BROKER_PORT = getenv('BROKER_PORT', 5672)
-BROKER_URL = 'amqp://guest:guest@{0}:{1}//'.format(BROKER_SERVER, BROKER_PORT)
+BROKER_VHOST = getenv('BROKER_VHOST', '/')
+BROKER_USER = getenv('BROKER_USER', 'guest')
+BROKER_PASS = getenv('BROKER_PASS', 'guest')
+BROKER_URL = 'amqp://{user}:{password}@{server}:{port}/{vhost}'.format(
+    user=BROKER_USER,
+    password=BROKER_PASS,
+    server=BROKER_SERVER,
+    port=BROKER_PORT,
+    vhost=BROKER_VHOST
+)
 CELERY_RESULT_BACKEND = 'amqp'
 
 CELERYD_STATE_DB = 'celery_worker_state'
