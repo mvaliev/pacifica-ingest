cc1b392272615df4e670367fb9ed1c84ab849201
David Brown <dmlb2000@gmail.com>
2017-12-12 11:11:08 -0800

    move celery settings to backend where it's required (#49)
    * move celery settings to backend where it's required
    
    * try importing relative
    
    * remove settings from static analysis

--- ingest/backend/celery_ingest.py
+++ ingest/backend/celery_ingest.py
@@ -3,11 +3,12 @@
 """Primary celery process."""
 from __future__ import absolute_import
 from celery import Celery
+from . import settings
 
 
 INGEST_APP = Celery('ingest_backend', include=['ingest.backend.tasks'])
 
-INGEST_APP.config_from_object('settings')
+INGEST_APP.config_from_object(settings)
 
 # Optional configuration, see the application user guide.
 INGEST_APP.conf.update(
