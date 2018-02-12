13cd4521ee98ae0c1f363d3ff6ada21364ca6c68
David Brown <dmlb2000@gmail.com>
2018-02-12 14:38:42 -0800

    add find_packages method and tab complete setup method (#57)
    * add find_packages method and tab complete setup method
    
    * use scm versioning

--- setup.py
+++ setup.py
@@ -2,22 +2,25 @@
 # -*- coding: utf-8 -*-
 """Setup and install the ingest."""
 from pip.req import parse_requirements
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
-setup(name='PacificaIngest',
-      version='1.0',
-      description='Pacifica Ingest',
-      author='David Brown',
-      author_email='david.brown@pnnl.gov',
-      packages=['ingest', 'ingest.backend', 'ingest.test'],
-      scripts=['IngestServer.py', 'DatabaseCreate.py'],
-      entry_points={
-          'console_scripts': [
-              'IngestCMD=ingest.__main__:cmd',
-              'IngestServer=ingest.__main__:main'
-          ]
-      },
-      install_requires=[str(ir.req) for ir in INSTALL_REQS])
+setup(
+    name='PacificaIngest',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Pacifica Ingest',
+    author='David Brown',
+    author_email='david.brown@pnnl.gov',
+    packages=find_packages(),
+    scripts=['IngestServer.py', 'DatabaseCreate.py'],
+    entry_points={
+        'console_scripts': [
+            'IngestCMD=ingest.__main__:cmd',
+            'IngestServer=ingest.__main__:main'
+        ]
+    },
+    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+)
