a31fbf17f55044780944e0cdc8ded6c06f0eb33f
David Brown <dmlb2000@gmail.com>
2018-11-28 19:59:15 -0800

    fix #68 add PEEWEE_URL env (#69)
    This adds a PEEWEE_URL default environment variable.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- pacifica/ingest/config.py
+++ pacifica/ingest/config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Configuration reading and validation module."""
+from os import getenv
 try:
     from ConfigParser import SafeConfigParser
 except ImportError:  # pragma: no cover python 2 vs 3 issue
@@ -10,9 +11,9 @@ from pacifica.ingest.globals import CONFIG_FILE
 
 def get_config():
     """Return the ConfigParser object with defaults set."""
-    configparser = SafeConfigParser({
-        'peewee_url': 'sqliteext:///db.sqlite3'
-    })
+    configparser = SafeConfigParser()
     configparser.add_section('database')
+    configparser.set('database', 'peewee_url', getenv(
+        'PEEWEE_URL', 'sqliteext:///db.sqlite3'))
     configparser.read(CONFIG_FILE)
     return configparser
