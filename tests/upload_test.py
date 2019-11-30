8b0d4abe7e4b9dddd23f86d5ac9bd75b9d470892
Marat Valiev <marat.valiev@gmail.com>
2019-11-30 10:59:50 -0800

    Add data staging

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -3,8 +3,53 @@
 """Test ingest with good uploads of good and bad data."""
 from __future__ import print_function, absolute_import
 import requests
-from common_methods_test import try_good_upload
+from common_methods_test import try_good_upload, try_good_upload1
 from make_data_test import make_data
+from contextlib import contextmanager
+import tarfile
+from os.path import join, dirname, abspath
+from os import remove
+from shutil import copy, Error
+
+_THIS_DIR = dirname(abspath(__file__))
+_DATA_DIR = join(_THIS_DIR, 'test_data')
+_META_DATA_DIR = join(_DATA_DIR, 'metadata-files')
+
+
+@contextmanager
+def test_data(prefix, custom_meta_file=None):
+    """generate data for tests."""
+    if custom_meta_file:
+        meta_file_in = join(_META_DATA_DIR, custom_meta_file)
+    else:
+        meta_file_in = join(_META_DATA_DIR, '%s-md.json' % prefix)
+    meta_file = join(_DATA_DIR, 'metadata.txt')
+    tar_file_out = join(_DATA_DIR, '%s.tar' % prefix)
+    data_dir = join(_DATA_DIR, 'data')
+
+    try:
+        copy(meta_file_in, meta_file)
+    except Error:
+        print('problems copying metafile')
+        raise
+
+    with tarfile.open(tar_file_out, mode='w') as tfo:
+
+        try:
+            tfo.add(meta_file)
+        except tarfile.TarError:
+            print('cannot add metafile to tar package')
+            raise
+
+        try:
+            tfo.add(data_dir)
+        except tarfile.TarError:
+            print('cannot add data dir to tar package')
+            raise
+
+    yield abspath(tar_file_out)
+
+    remove(tar_file_out)
 
 
 def test_bad_job_id():
@@ -35,8 +80,9 @@ def test_bad_metadata_upload():
 
 def test_bad_json_upload():
     """Test if the metadata is down."""
-    make_data('bad-json', 'bad-json-md.notjson')
-    try_good_upload('bad-json', 'FAILED', 'load metadata', 0)
+    # make_data('bad-json', 'bad-json-md.notjson')
+    with test_data('bad-json', 'bad-json-md.notjson') as fpath:
+        try_good_upload1(fpath, 'FAILED', 'load metadata', 0)
 
 
 def test_bad_tarfile_upload():
