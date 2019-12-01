b507ad7e797664644ed98ff031b11b765b8f97ef
Marat Valiev <marat.valiev@gmail.com>
2019-11-30 16:45:31 -0800

    Restoring working files from dev.

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -2,8 +2,49 @@
 # -*- coding: utf-8 -*-
 """Test ingest with good uploads of good and bad data."""
 from __future__ import print_function, absolute_import
+from contextlib import contextmanager
+import tarfile
+from os.path import join, dirname, abspath
+from os import remove
+from shutil import copy, Error
 import requests
 from common_methods_test import try_good_upload
+# from common_methods_test import try_good_upload1
+
+_THIS_DIR = dirname(abspath(__file__))
+_DATA_DIR = join(_THIS_DIR, 'test_data')
+_META_DATA_DIR = join(_DATA_DIR, 'metadata-files')
+
+
+@contextmanager
+def test_data(prefix, custom_meta_file=None):
+    """generate data for tests."""
+    if custom_meta_file:
+        meta_file_in = join(_META_DATA_DIR, custom_meta_file)
+    else:
+        meta_file_in = join(_META_DATA_DIR, '%s-md.json' % prefix)
+    meta_file = join(_DATA_DIR, 'metadata.txt')
+    tar_file_out = join(_DATA_DIR, '%s.tar' % prefix)
+    data_dir = join(_DATA_DIR, 'data')
+
+    try:
+        copy(meta_file_in, meta_file)
+    except Error:
+        print('problems copying metafile')
+        raise
+
+    with tarfile.open(tar_file_out, mode='w') as tfo:
+
+        try:
+            tfo.add(meta_file)
+            tfo.add(data_dir)
+        except tarfile.TarError:
+            print('cannot create tar package')
+            raise
+
+    yield abspath(tar_file_out)
+
+    remove(tar_file_out)
 
 
 def test_bad_job_id():
