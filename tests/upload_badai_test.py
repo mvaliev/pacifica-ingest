55fa927a404aa08c8cf3d31c4325b3c2ad7223d2
Marat Valiev <marat.valiev@gmail.com>
2019-12-03 08:51:49 -0800

    Fix bad_ai error

--- tests/upload_badai_test.py
+++ tests/upload_badai_test.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 """Test ingest with a disabled archive interface."""
 from __future__ import print_function, absolute_import
-from common_methods_test import try_good_upload, try_good_move
+from common_methods_test import try_good_upload1, try_good_move
+from upload_test import data_load
 
 
 # this is a long name but descriptive.
 # pylint: disable=invalid-name
 def test_bad_archiveinterface_upload():
     """Test if the archive interface is down."""
-    try_good_upload('good', 'FAILED', 'ingest files', 0, 10)
+    with data_load('good') as fpath:
+        try_good_upload1(fpath, 'FAILED', 'ingest files', 0, 10)
+    # try_good_upload('good', 'FAILED', 'ingest files', 0, 10)
 # pylint: enable=invalid-name
 
 
