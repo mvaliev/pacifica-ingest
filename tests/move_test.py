05facf5f23b3cdecce3a1f3f68f341a9bc735121
Marat Valiev <marat.valiev@gmail.com>
2019-12-03 14:48:49 -0800

    Increase wait time.

--- tests/move_test.py
+++ tests/move_test.py
@@ -70,5 +70,5 @@ def test_big_move():
             )
             assert req.status_code == 200
             job_id = req.json()['job_id']
-            job_state = check_upload_state(job_id, 12)
+            job_state = check_upload_state(job_id, 60)
             try_assert_job_state(job_state, 'OK', 'ingest metadata', 100)
