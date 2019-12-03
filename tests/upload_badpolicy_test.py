90240ccf80001fe842bd770c050f4d5b3b0c9fe5
Marat Valiev <marat.valiev@gmail.com>
2019-12-02 23:39:24 -0800

    Fix bad policy error.

--- tests/upload_badpolicy_test.py
+++ tests/upload_badpolicy_test.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 """Test ingest with a disabled policy server."""
 from __future__ import print_function, absolute_import
-from common_methods_test import try_good_upload
-
+from common_methods_test import try_good_upload1
+from upload_test import data_load
 
 def test_bad_policy_upload():
     """Test if the policy server is down."""
-    try_good_upload('good', 'FAILED', 'Policy Validation', 0, 10)
+    with data_load('good') as fpath:
+        try_good_upload1(fpath, 'FAILED', 'Policy Validation', 0, 10)
+    # try_good_upload('good', 'FAILED', 'Policy Validation', 0, 10)
