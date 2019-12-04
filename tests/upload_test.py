1f0ce23944e8cbe8414dac920261c8d9f67520a1
Marat Valiev <marat.valiev@gmail.com>
2019-12-04 09:42:13 -0800

    Refactor data generation.

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -16,53 +16,6 @@ _DATA_DIR = join(_THIS_DIR, 'test_data')
 _META_DATA_DIR = join(_DATA_DIR, 'metadata-files')
 
 
-@contextlib.contextmanager
-def data_load(prefix, custom_meta_file=None):
-    """
-    Generate data for tests.
-
-    It assumed that data folder is located in tests/test_data
-    and that metadata files are located in tests/test_data/metadata-files.
-
-    Parameters:
-        prefix (str): metadata file prefix (*-md.json)
-        custom_meta_file (str): custom name for metadata file
-    """
-    if custom_meta_file:
-        meta_file_in = join(_META_DATA_DIR, custom_meta_file)
-    else:
-        meta_file_in = join(_META_DATA_DIR, '%s-md.json' % prefix)
-
-    old_dir = getcwd()
-    work_dir = _DATA_DIR
-    chdir(work_dir)
-    meta_file = 'metadata.txt'
-    bundle = '%s.tar' % prefix
-    data_dir = 'data'
-
-    try:
-        copy(meta_file_in, meta_file)
-    except OSError:
-        print('\n problems copying metafile \n%s' % meta_file_in)
-        raise
-
-    with tarfile.open(bundle, mode='w') as tfo:
-        try:
-            tfo.add(meta_file)
-            tfo.add(data_dir)
-        except tarfile.TarError:
-            print('cannot create tar package')
-            raise
-
-    try:
-        yield join(work_dir, bundle)
-    finally:
-        chdir(work_dir)
-        remove(bundle)
-        remove(meta_file)
-        chdir(old_dir)
-
-
 def data_up(prefix, custom_meta_file=None):
     """
     Generate data for tests.
@@ -106,7 +59,32 @@ def data_up(prefix, custom_meta_file=None):
     return join(work_dir, bundle)
 
 
-def data_up1(prefix, custom_meta_file=None):
+@contextlib.contextmanager
+def data_load(prefix, custom_meta_file=None):
+    """
+    Generate data for tests.
+
+    It assumed that data folder is located in tests/test_data
+    and that metadata files are located in tests/test_data/metadata-files.
+
+    Parameters:
+        prefix (str): metadata file prefix (*-md.json)
+        custom_meta_file (str): custom name for metadata file
+    """
+
+    fpath = data_up(prefix, custom_meta_file)
+
+    try:
+        yield fpath
+    finally:
+        try:
+            remove(fpath)
+        except OSError:
+            print("Warning: test tarfile was already removed")
+
+
+@contextlib.contextmanager
+def data_load1(prefix, custom_meta_file=None):
     """
     Generate data for tests.
 
@@ -143,10 +121,13 @@ def data_up1(prefix, custom_meta_file=None):
             print('cannot create tar package')
             raise
 
-    chdir(old_dir)
-
-    return join(work_dir, bundle)
-
+    try:
+        yield join(work_dir, bundle)
+    finally:
+        chdir(work_dir)
+        remove(bundle)
+        remove(meta_file)
+        chdir(old_dir)
 
 
 def test_bad_job_id():
